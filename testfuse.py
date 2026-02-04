97099a6580d98154374e0106b173ea3676fc2919
VIRUSGAMING64 <virusroger6@gmail.com>
2026-02-04 11:03:29 -0500

    3.12.0.0

---
+++ testfuse.py
@@ -0,0 +1,35 @@
+import asyncio
+try:
+    asyncio.get_event_loop()
+except RuntimeError as e:
+    print(e)
+    asyncio.set_event_loop(asyncio.new_event_loop())
+from dotenv import load_dotenv
+load_dotenv()
+from modules.fuse.tree import *
+from pyrogram import *
+import os
+API_ID=os.getenv("API_ID")
+API_HASH=os.getenv("API_HASH")
+TOKEN = os.getenv("TOKEN")
+
+bot = Client(
+    "fuse_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN
+)
+
+
+bot.start()
+f=file(bot,1659735368)
+
+f.max_size = 4
+
+f.write(b"hello world")
+
+f.name = "candela"
+
+f.created_at = datetime.datetime.now().isoformat()
+
+f.close()
